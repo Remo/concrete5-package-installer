4d6a88ce659b6425b3db15d1e442a72252a12c08
remolaubacher <remo.laubacher@gmail.com>
2015-06-11 17:04:26 +0200

    remove unused import

--- controllers/single_page/dashboard/extend/import_zip.php
+++ controllers/single_page/dashboard/extend/import_zip.php
@@ -5,7 +5,6 @@ namespace Concrete\Package\PackageInstaller\Controller\SinglePage\Dashboard\Exte
 use Core,
     Loader,
     Package,
-    Less_Parser,
     \Concrete\Core\Page\Controller\DashboardPageController;
 
 class ImportZip extends DashboardPageController
@@ -16,7 +15,7 @@ class ImportZip extends DashboardPageController
         if ($zip->open($_FILES['file']['tmp_name']) === TRUE) {
             $zip->extractTo(DIR_PACKAGES . '/' . basename($_FILES['file']['name'], '.zip'));
             $zip->close();
-            
+
             $this->redirect('/dashboard/extend/install');
         }
         die();
